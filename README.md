7cd5b116ca746d47654933cdb7658833b5a172f9
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-13 16:01:47 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express Layering Example
+
+An example of organizing express using express router and layering in the file structure.
---
+++ src/store.js
@@ -0,0 +1,14 @@
+const DATA = {
+  cards: [{
+    id: 1,
+    title: 'Task One',
+    content: 'This is card one'
+  }],
+  list: [{
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+  }]
+}
+
+module.exports = { DATA }
